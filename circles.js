470584d7107508715170311c05efe1e4f24054d1
Online MacBook <onlinemacbook@Onlines-MacBook-Pro.local>
2019-06-09 12:17:47 -0400

    added the diameter attribute to the circle class function

--- circles.js
+++ circles.js
@@ -1,16 +1,17 @@
 class Circles {
 
-  constructor(x, y, r, g, b) {
+  constructor(x, y, r, g, b, d) {
     this.x = x;
     this.y = y;
     this.r = r;
     this.g = g;
     this.b = b;
+    this.d = d;
   }
 
   drawCircles() {
     noStroke()
     fill(this.r, this.g, this.b)
-    circle(this.x, this.y, 95)
+    circle(this.x, this.y, this.d)
   }
 }
